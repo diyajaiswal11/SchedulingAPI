d9f6d5766aac5a8781e55fbecb5008cdde1cc525
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-06-17 01:45:02 +0530

    made changes

--- schedulingapp/urls.py
+++ schedulingapp/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    path('',views.home,name='home'),
-    
+    path('',views.home,name='home'), #makes call to API
+    path('ping/',views.ping,name='ping'), #sends GET request to /ping endpoint
+    path('test/<str:url>/<str:date>/',views.test,name='test'), #API call having two parameters, url and date-time
+    path('<str:url>/',views.test1,name='test1'), #GET request to url specified
 ]
--- schedulingapp/views.py
+++ schedulingapp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from datetime import datetime
+from django.shortcuts import render,reverse
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+
+def test1(request,url):
+    return HttpResponse(status=200)
+    #get request on url specified returns status code 200
+
+def test(request,url,date):
+    now=datetime.now()
+    s=str(now.year)+"-"+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+'-'+str(now.minute)
+    if str(date)==str(s):
+        return HttpResponseRedirect((reverse('test1',args=(url,))))
+        #when current Date-Time matches the one specified in Date-Time parameter, get request sent to specified url parameter
+    else:
+        return HttpResponse(status=404)
+        #if current Date-Time does not match the one specified in Date-Time parameter, status code 400 returned
 
 def home(request):
-    return HttpResponse("Hello")
+    return HttpResponseRedirect((reverse('test',args=('tryurl','2020-6-17-1-41',))))
+    #call made to API with two parameters, url and date time
+
+
+def ping(request):
+    return JsonResponse({"status":"OK"})
+    #when GET request is sent to the '/ping' endpoint, server returns JSON "{ "status":"OK"}"
+
